48d9f588bf2a23ef731ea5de7b19b6c0a2c109fd
lucasmmoura09 <morais.lucas@discente.ufma.br>
2026-05-03 14:58:47 -0300

    Update Appy.py

--- Appy.py
+++ Appy.py
@@ -1,43 +1,57 @@
 print("=== LOJA ===")
 
+# entrada (str)
 nome = input("Digite seu nome: ")
 
-print("1 - Camisa (50)")
-print("2 - Calça (100)")
-print("3 - Tênis (200)")
+print("\n1 - Camisa (50.0)")
+print("2 - Calça (100.0)")
+print("3 - Tênis (200.0)")
 
-opcao = int(input("Escolha: "))
+# entrada (int)
+opcao = int(input("Escolha o produto: "))
 quantidade = int(input("Quantidade: "))
 
-# switch (match-case)
+# variáveis
+produto = ""
+preco = 0.0   # float
+desconto = False  # bool
+
+# ----------------------------
+# SWITCH (match-case)
+# ----------------------------
 match opcao:
     case 1:
         produto = "Camisa"
-        preco = 50
+        preco = 50.0
     case 2:
         produto = "Calça"
-        preco = 100
+        preco = 100.0
     case 3:
         produto = "Tênis"
-        preco = 200
+        preco = 200.0
     case _:
         print("Opção inválida")
         exit()
 
+# processamento
 total = preco * quantidade
 
+# ----------------------------
+# IF / ELSE (condição)
+# ----------------------------
 if total > 100:
     total = total * 0.9
     desconto = True
 else:
     desconto = False
 
-print("\nResumo:")
+# saída
+print("\n=== RESUMO ===")
 print("Cliente:", nome)
 print("Produto:", produto)
 print("Quantidade:", quantidade)
 
 if desconto:
-    print("Desconto aplicado")
+    print("Desconto aplicado: 10%")
 
 print("Total:", total)
